db07191cd57387b821a3af23285ceaa8eee7767f
Ausath Abdi Dzil Ikram <147676922+ausathdzil@users.noreply.github.com>
2024-11-22 20:30:41 +0700

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 function App() {
   return (
     <main>
-      <h1>Development Branch</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
       <p>Hello World</p>
+      <Outlet />
     </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './routes/About.jsx';
+import Contact from './routes/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
